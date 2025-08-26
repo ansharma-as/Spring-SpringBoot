6e834636d50c55d05db124fa9044bf66254299ac
ansharma-as <anshsharma712.as@gmail.com>
2025-08-26 09:04:30 +0530

    encapsulation.java

---
+++ OOPS/Encapsullation/Ensapsulation.java
@@ -0,0 +1,8 @@
+public class Ensapsulation {
+    public static void main(String[] args){
+        
+    }
+    public void dance(){
+            System.out.println("dancing....");
+    }
+}
--- OOPS/main.java
+++ OOPS/main.java
@@ -1,16 +1,31 @@
 class Main{
     public static void main(String[] args) {
-        Person p1 = new Person(); // using default constructor here
-        p1.name = "Ansh";
-        p1.age = 18;
-        p1.talk();
-        System.out.println(p1.name + " is " + p1.age + " years old.");
+        // Person p1 = new Person(); // using default constructor here
+        // p1.name = "Ansh";
+        // p1.age = 18;
+        // p1.talk();
+        // System.out.println(p1.name + " is " + p1.age + " years old.");
 
 
 
-        Person p2 = new Person(25, "Alice"); // using parameterized constructor
-        System.out.println(p2.name + " is " + p2.age + " years old.");
-        System.out.println("Total persons created: " + Person.count);
+        // Person p2 = new Person(25, "Alice"); // using parameterized constructor
+        // System.out.println(p2.name + " is " + p2.age + " years old.");
+        // System.out.println("Total persons created: " + Person.count);
+
+        // Developer dev = new Developer(30, "Bob");
+        // dev.walk();
+        // System.out.println(dev.name + " is " + dev.age + " years old.");
+
+        Ensapsulation encap = new Ensapsulation();
+        encap.setName("John");
+        encap.setAge(25);
+        encap.dance();
+    }
+}
+
+class Developer extends Person{
+    Developer(int age , String name){
+        super(age, name);
     }
 }
 
@@ -32,5 +47,9 @@ class Person{
 
     void talk(){
         System.out.println(name + " is talking.");
+    }
+
+    void walk() {
+        System.out.println(name + " is walking.");
     }
 }
