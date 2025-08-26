a85d3833ab5e0724ecbc1c3b2dd49818ab3e0380
ansharma-as <anshsharma712.as@gmail.com>
2025-08-26 09:04:53 +0530

    encapsulation.java

---
+++ Basics/Array.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Array {
+
+    public static void main(String[] args){
+        ArrayList<Integer> arr1= new ArrayList<Integer>();
+        arr1.add(10);
+        arr1.add(20);
+        arr1.add(30);
+        System.out.println(arr1);
+        
+        for (int arr : arr1){
+            System.out.println(arr);
+        }
+
+       }   }
